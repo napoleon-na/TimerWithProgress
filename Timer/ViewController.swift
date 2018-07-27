9a6fe474ba7baf6036fd31fc42005167698e0f79
Naomi Watanabe <napoleon.na1120@gmail.com>
2018-07-27 13:58:08 +0900

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Naomi Watanabe on 2018/07/27.
+//  Copyright © 2018年 N Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
